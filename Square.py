5f6f432656d4f8f38ec1e26d29c12af77c84de74
vxf604 <vxf604@alumni.ku.dk>
2025-09-03 13:04:08 +0200

    square

---
+++ Square.py
@@ -0,0 +1,23 @@
+from time import sleep
+
+import robot
+
+# Create a robot object and initialize
+arlo = robot.Robot()
+leftSpeed = 65
+rightSpeed = 64
+
+print("Running ...")
+for i in range (4):
+    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+    sleep(2.45)
+    print(arlo.stop())
+    print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+    
+
+# 2m = 4,9 seconds
+# 1m = 2,45 seconds
+
+# send a stop command
+print(arlo.stop())
+print("Finished")
