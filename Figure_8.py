1634541ee81eab2e5519eb02698e1c0f2934a9c9
MariusGate3 <54096802+MariusGate3@users.noreply.github.com>
2025-09-03 16:38:14 +0200

    asd2

--- Figure_8.py
+++ Figure_8.py
@@ -13,7 +13,7 @@ while loops < 3:
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
     sleep(7.45)
     print(arlo.go_diff(rightSpeed, leftSpeed, 1, 1))
-    sleep(8.5)
+    sleep(8.2)
     loops += 1
 
 # 2m = 4,9 seconds
