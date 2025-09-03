c4e238f9fed378a5218a916a4a58d7e12a80d3a1
vxf604 <vxf604@alumni.ku.dk>
2025-09-03 13:20:08 +0200

    j

--- Square.py
+++ Square.py
@@ -7,16 +7,16 @@ arlo = robot.Robot()
 leftSpeed = 65
 rightSpeed = 64
 
-# print("Running ...")
-# for i in range (4):
-#     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-#     sleep(2.45)
-#     print(arlo.stop())
-#     print(arlo.go_diff(leftSpeed, rightSpeed, 0.8, 1))
-#     sleep(0.5)
+print("Running ...")
+for i in range (4):
+    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+    sleep(2.45)
+    print(arlo.stop())
+    print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+    sleep(0.5)
 
-while True:
-    print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))    
+# while True:
+#     print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))    
 
 # 2m = 4,9 seconds
 # 1m = 2,45 seconds
