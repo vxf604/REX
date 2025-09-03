0b3df8025fa1eca7da0e27645e590494c3fedc1f
MariusGate3 <54096802+MariusGate3@users.noreply.github.com>
2025-09-03 16:58:54 +0200

    asd2

--- Figure_8.py
+++ Figure_8.py
@@ -11,7 +11,7 @@ print("Running ...")
 loops = 0
 while loops < 3:
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-    sleep(7.46)
+    sleep(7.475)
     print(arlo.go_diff(rightSpeed, leftSpeed, 1, 1))
     sleep(8.5)
     loops += 1
