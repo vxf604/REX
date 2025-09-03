7f862ff8be61eb5ecc488b8e5647566db701e968
vxf604 <vxf604@alumni.ku.dk>
2025-09-03 14:24:42 +0200

    asd2

--- Square.py
+++ Square.py
@@ -12,17 +12,15 @@ rightSpeed = 64
 
 
 print("Running ...")
-# for i in range (4):
-#     print(arlo.drive_forward_meter(1, leftSpeed, rightSpeed))
-#     print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
-#     sleep(0.73)
+for i in range (4):
+    print(arlo.drive_forward_meter(1, leftSpeed, rightSpeed))
+    print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+    sleep(0.711)
 
-print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
-sleep(0.711)
     
     
 # 3.61 - 6.86 = 3.25
-# 90 degree = 0.8125
+# 90 degree = 0.711
 
 
  
