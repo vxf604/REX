854692f0b2a6d90f3f2f3f2036d81208df6cbbeb
vxf604 <vxf604@alumni.ku.dk>
2025-09-03 13:43:25 +0200

    j

--- Square.py
+++ Square.py
@@ -12,17 +12,19 @@ rightSpeed = 64
 
 
 print("Running ...")
-for i in range (4):
-    print(arlo.drive_forward_meter(1))
-    print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
-    sleep(0.5)
+# for i in range (4):
+#     print(arlo.drive_forward_meter(1))
+#     print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+#     sleep(0.5)
+
+    
     
     
     
 
 
-# while True:
-#     print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))    
+while True:
+    print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))    
 
 # 2m = 4,9 seconds
 # 1m = 2,45 seconds
