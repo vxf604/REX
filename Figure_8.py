41c6c0141ed2127cf7b024a2d6f6dfd2863fe5c7
vxf604 <vxf604@alumni.ku.dk>
2025-09-03 15:40:47 +0200

    asd2

---
+++ Figure_8.py
@@ -0,0 +1,21 @@
+from time import sleep
+
+import robot
+
+# Create a robot object and initialize
+arlo = robot.Robot()
+leftSpeed = 67
+rightSpeed = 64
+
+print("Running ...")
+
+
+print(arlo.go_diff(leftSpeed, rightSpeed, 0.5, 1))
+sleep(2)
+
+
+
+
+# 2m = 4,9 seconds
+# 1m = 2,45 seconds
+print("Finished")
