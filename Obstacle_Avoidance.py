32589c115cb4c0f1800523271afe9ecfca422398
MariusGate3 <54096802+MariusGate3@users.noreply.github.com>
2025-09-08 16:48:54 +0200

    asdasd

--- Obstacle_Avoidance.py
+++ Obstacle_Avoidance.py
@@ -25,17 +25,17 @@ while driving:
         print(arlo.stop())
         if rightSensor > leftSensor:
             print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
-            while frontSensor < 50:
+            while frontSensor < 150:
                 frontSensor = arlo.read_front_ping_sensor()
                 print("turning left until path clear")
         elif leftSensor > rightSensor:
             print(arlo.go_diff(rightSpeed, leftSpeed, 0, 1))
-            while frontSensor < 50:
+            while frontSensor < 150:
                 frontSensor = arlo.read_front_ping_sensor()
                 print("turning right until path clear")
         else:
             print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
-            while frontSensor < 50:
+            while frontSensor < 150:
                 frontSensor = arlo.read_front_ping_sensor()
                 print("turning left until path clear")
         sleep(0.4)
