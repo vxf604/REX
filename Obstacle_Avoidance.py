dad78adc95b924328aa1b27a586ba0139773c84e
MariusGate3 <54096802+MariusGate3@users.noreply.github.com>
2025-09-08 15:48:31 +0200

    sens

--- Obstacle_Avoidance.py
+++ Obstacle_Avoidance.py
@@ -13,13 +13,17 @@ driving = True
 
 while driving:
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+    leftSensor = arlo.read_left_ping_sensor()
+    rightSensor = arlo.read_right_ping_sensor()
+    print("Left Sensor: ", leftSensor)
+    print("Right Sensor: ", rightSensor)
     distance = arlo.read_front_ping_sensor()
-    print("Distance: ", distance)
-    if distance < 500:
-        print(arlo.stop())
-        driving = False
-        
+    if leftSensor > rightSensor:
+        leftSpeed += 2
+        rightSpeed -= 2
+    elif rightSensor > leftSensor:
+        leftSpeed -= 2
+        rightSpeed += 2
 
-    
 
 print("Finished")
