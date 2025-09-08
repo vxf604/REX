d054a213bb9ed149a441a4a002229084eecb926c
vxf604 <vxf604@alumni.ku.dk>
2025-09-08 15:33:18 +0200

    Add

--- Obstacle_Avoidance.py
+++ Obstacle_Avoidance.py
@@ -12,7 +12,6 @@ rightSpeed = 64
 distance = 200
 
 while distance > 100:
-    print(arlo.drive_forward_meter(2, leftSpeed, rightSpeed))
     distance = arlo.read_front_ping_sensor
     print("Distance: ", distance)
 
