e5c2ae70ba01e4714fa1b0025e7f8e22688c44d0
MariusGate3 <54096802+MariusGate3@users.noreply.github.com>
2025-09-08 16:03:20 +0200

    sens

--- Obstacle_Avoidance.py
+++ Obstacle_Avoidance.py
@@ -1,3 +1,4 @@
+from asyncio import sleep
 import robot
 
 # Create a robot object and initialize
@@ -11,23 +12,37 @@ leftSpeed = 67
 rightSpeed = 64
 driving = True
 
+# while driving:
+#     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+#     leftSensor = arlo.read_left_ping_sensor()
+#     rightSensor = arlo.read_right_ping_sensor()
+#     frontSensor = arlo.read_front_ping_sensor()
+#     print("Left Sensor: ", leftSensor)
+#     print("Right Sensor: ", rightSensor)
+#     print("Front Sensor: ", frontSensor)
+#     distance = arlo.read_front_ping_sensor()
+#     if frontSensor < 200:
+#         leftSpeed += 3
+#     elif leftSensor > rightSensor:
+#         leftSpeed += 1
+#         rightSpeed -= 1
+#     elif rightSensor > leftSensor:
+#         leftSpeed -= 1
+#         rightSpeed += 1
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
 while driving:
-    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-    leftSensor = arlo.read_left_ping_sensor()
-    rightSensor = arlo.read_right_ping_sensor()
     frontSensor = arlo.read_front_ping_sensor()
-    print("Left Sensor: ", leftSensor)
-    print("Right Sensor: ", rightSensor)
+    rightSensor = arlo.read_right_ping_sensor()
+    leftSensor = arlo.read_left_ping_sensor()
     print("Front Sensor: ", frontSensor)
-    distance = arlo.read_front_ping_sensor()
-    if frontSensor < 200:
-        leftSpeed += 3
-    elif leftSensor > rightSensor:
-        leftSpeed += 1
-        rightSpeed -= 1
-    elif rightSensor > leftSensor:
-        leftSpeed -= 1
-        rightSpeed += 1
+    print("Right Sensor: ", rightSensor)
+    print("Left Sensor: ", leftSensor)
+
+    if frontSensor < 150:
+        print(arlo.stop())
+        print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+        sleep(0.711)
+        print(arlo.go_diff(rightSpeed, leftSpeed, 1, 1))
 
 
 print("Finished")
