7a7ce751e5d6d8d6351f7b603697ee177266e5fa
vxf604 <vxf604@alumni.ku.dk>
2025-09-10 15:30:13 +0200

    code

--- Obstacle_Avoidance.py
+++ Obstacle_Avoidance.py
@@ -1,4 +1,3 @@
-from time import sleep
 import robot
 
 # Create a robot object and initialize
@@ -12,58 +11,39 @@ leftSpeed = 67
 rightSpeed = 64
 driving = True
 
-print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 while driving:
-    frontSensor = arlo.read_front_ping_sensor()
-    rightSensor = arlo.read_right_ping_sensor()
+    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
     leftSensor = arlo.read_left_ping_sensor()
-    print("Front Sensor: ", frontSensor)
-    print("Right Sensor: ", rightSensor)
+    rightSensor = arlo.read_right_ping_sensor()
     print("Left Sensor: ", leftSensor)
-
-    if frontSensor < 250 or rightSensor < 50 or leftSensor < 50:
-        print(arlo.stop())
-        if rightSensor > leftSensor:
-            print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
-            while frontSensor < 150:
-                frontSensor = arlo.read_front_ping_sensor()
-                print("turning left until path clear")
-        elif leftSensor > rightSensor:
-            print(arlo.go_diff(rightSpeed, leftSpeed, 0, 1))
-            while frontSensor < 150:
-                frontSensor = arlo.read_front_ping_sensor()
-                print("turning right until path clear")
-        else:
-            print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
-            while frontSensor < 150:
-                frontSensor = arlo.read_front_ping_sensor()
-                print("turning left until path clear")
-        sleep(0.4)
-        print(arlo.go_diff(rightSpeed, leftSpeed, 1, 1))
-
-# print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-# while driving:
-#     frontSensor = arlo.read_front_ping_sensor()
-#     rightSensor = arlo.read_right_ping_sensor()
-#     leftSensor = arlo.read_left_ping_sensor()
-#     print("Front Sensor: ", frontSensor)
-#     print("Right Sensor: ", rightSensor)
-#     print("Left Sensor: ", leftSensor)
-
-#     if frontSensor < 300 or rightSensor < 150 or leftSensor < 150:
-#         if rightSensor < leftSensor:
-#             leftSpeed -= 40
-#             rightSpeed += 40
-#             print("Turning left")
-#         elif leftSensor < rightSensor:
-#             rightSpeed -= 40
-#             leftSpeed += 40
-#             print("Turning right")
-#         else:
-#             rightSpeed += 40
-#             leftSpeed -= 40
-#             print("Obstacle in front, Turning right")
-#     sleep(0.1)
+    print("Right Sensor: ", rightSensor)
+    distance = arlo.read_front_ping_sensor()
+    if leftSensor > rightSensor:
+        leftSpeed += 2
+        rightSpeed -= 2
+    elif rightSensor > leftSensor:
+        leftSpeed -= 2
+        rightSpeed += 2
 
 
 print("Finished")
