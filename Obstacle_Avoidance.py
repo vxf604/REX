3ddaf99df806858f81477a49398865a01771e43a
vxf604 <vxf604@alumni.ku.dk>
2025-09-15 15:04:28 +0200

    add

--- Obstacle_Avoidance.py
+++ Obstacle_Avoidance.py
@@ -37,29 +37,7 @@ while driving:
         sleep(0.4)
         print(arlo.go_diff(rightSpeed, leftSpeed, 1, 1))
 
-# print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-# while driving:
-#     frontSensor = arlo.read_front_ping_sensor()
-#     rightSensor = arlo.read_right_ping_sensor()
-#     leftSensor = arlo.read_left_ping_sensor()
-#     print("Front Sensor: ", frontSensor)
-#     print("Right Sensor: ", rightSensor)
-#     print("Left Sensor: ", leftSensor)
 
-#     if frontSensor < 300 or rightSensor < 150 or leftSensor < 150:
-#         if rightSensor < leftSensor:
-#             leftSpeed -= 40
-#             rightSpeed += 40
-#             print("Turning left")
-#         elif leftSensor < rightSensor:
-#             rightSpeed -= 40
-#             leftSpeed += 40
-#             print("Turning right")
-#         else:
-#             rightSpeed += 40
-#             leftSpeed -= 40
-#             print("Obstacle in front, Turning right")
-#     sleep(0.1)
 
 
 print("Finished")
