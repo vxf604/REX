a9b7772df2a613b127853f4fd924c5aeacdb16ef
MariusGate3 <54096802+MariusGate3@users.noreply.github.com>
2025-10-01 14:16:34 +0200

    initial attempt at path planning and following

--- PathPlanning.py
+++ PathPlanning.py
@@ -2,72 +2,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 from robot import Robot
 
-arlo = Robot()
 
-id_list = []
-landmarks = []
-radius = 180
-
-for i in range (len(ids)):
-    landmark_id = ids[i][0]
-    if landmark_id in id_list:
-        continue
-    
-    map_x = tvecs[i][0][0]
-    map_y = tvecs[i][0][2]
-    
-    
-    landmarks.append((landmark_id, map_x, map_y))
-    id_list.append(landmark_id)
-    
-def in_collision (path, landmarks, robot_radius=150):
+def in_collision(path, landmarks, robot_radius=150):
     for p in path:
         x, y = p
-        
+
         for landmark in landmarks:
             id, map_x, map_y, radius = landmark
-            distance = np.sqrt((x - map_x)**2 + (y - map_y)**2) #Euclidean distance
+            distance = np.sqrt(
+                (x - map_x) ** 2 + (y - map_y) ** 2
+            )  # Euclidean distance
             if distance <= radius + robot_radius:
                 return True
     return False
-    
 
-def follow_rrt_path(arlo,path):
+
+def follow_rrt_path(arlo, path):
     for i in range(1, len(path)):
-        start = path[i-1]
-        target = path [i]
-        
+        start = path[i - 1]
+        target = path[len(path) - i]
+
         dx = target[0] - start[0]
         dy = target[1] - start[1]
         angle_rad = np.arctan2(dy, dx)
         angle_deg = np.degrees(angle_rad)
-        
+
         arlo.rotate_robot(angle_deg)
-        
-        distance_m = np.sqrt(dx**2 + dy**2) / 1000 # to meter
-        
-        distance_travelled = 0
-        
-        while distance_travelled < distance_m:
-            arlo.drive_forward_meter(distance_m, leftspeed= 64,  rightspeed = 67)
-            distance_travelled += 0.2
-            
-            current_position = arlo.get_position()
-            if in_collision (current_position, landmarks):
-                print("Collision detected! Stopping.")
-                arlo.stop()
-                break
-            
 
-        
+        distance_m = np.sqrt(dx**2 + dy**2) / 1000  # to meter
 
-    
-        
-fig, ax = plt.subplots() 
+        arlo.drive_forward_meter(distance_m, leftspeed=64, rightspeed=67)
+
+
+fig, ax = plt.subplots()
 obstacle_x = []
 obstacle_y = []
 for landmark in landmarks:
     id, map_x, map_y, radius = landmark
-    circle = plt.Circle((map_x, map_y), radius, color='r', fill=False, linestyle='--', alpha=0.5)
-    
-    
+    circle = plt.Circle(
+        (map_x, map_y), radius, color="r", fill=False, linestyle="--", alpha=0.5
+    )
---
+++ path_planning.py
@@ -0,0 +1,216 @@
+import cv2
+import cv2.aruco as aruco
+import time
+from pprint import *
+import os
+import datetime
+import sys
+from time import sleep
+import robot
+import numpy as np
+import random
+
+try:
+    import picamera2  # type: ignore
+
+    print("Camera.py: Using picamera2 module")
+except ImportError:
+    print("Camera.py: picamera2 module not available")
+    exit(-1)
+
+arlo = robot.Robot()
+speed = 40
+leftSpeed = speed + 3
+rightSpeed = speed
+f = 1226.11  # pixels
+X = 145  # mm
+cWidth = 1640
+cHeight = 1232
+imageSize = (cWidth, cHeight)
+
+intrinsic_matrix = np.array([[f, 0, cWidth / 2], [0, f, cHeight / 2], [0, 0, 1]])
+distortion_coeffs = np.zeros((5, 1))
+
+FPS = 30
+cam = picamera2.Picamera2()
+frame_duration_limit = int(1 / FPS * 1000000)  # Microseconds
+# Change configuration to set resolution, framerate
+picam2_config = cam.create_video_configuration(
+    {"size": imageSize, "format": "RGB888"},
+    controls={
+        "FrameDurationLimits": (frame_duration_limit, frame_duration_limit),
+        "ScalerCrop": (0, 0, 3280, 2464),
+    },
+    queue=False,
+)
+cam.configure(picam2_config)
+
+
+print("Running ...")
+running = True
+
+
+fx = 1226.11
+fy = 1226.11
+cx = 1640 / 2
+cy = 1232 / 2
+landmark_radius = 1800
+
+cameraMatrix = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]], dtype=np.float32)
+distCoeffs = np.zeros((5, 1))
+
+
+def checkForLandmark():
+    os.makedirs("images", exist_ok=True)
+
+    image = cam.capture_array("main")
+    # timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    # image_path = os.path.join("images", f"captured_image_{timestamp}.png")
+    # cv2.imwrite(image_path, image)
+
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    parameters = aruco.DetectorParameters_create()
+    corners, ids, rejected = aruco.detectMarkers(
+        image, aruco_dict, parameters=parameters
+    )
+    if ids is None:
+        print(" No marker detected!")
+        return False, None
+
+    c = corners[0][0]  # first marker detected
+    x = int(cv2.norm(c[0] - c[1]))
+    Z = f * X / x
+    print(f"Distance to landmark Z: {Z} mm")
+    return True, corners, ids
+
+
+landmark_detected = False
+cam.start(show_preview=False)
+radius_landmark = 1800
+
+
+def distance(p1, p2):
+    return np.linalg.norm(np.array(p1) - np.array(p2))
+
+
+def in_collision(point, landmarks, robot_radius=150):
+    x, y = point
+
+    for landmark in landmarks:
+        id, map_x, map_y, radius = landmark
+        landmark_pos = (map_x, map_y)
+        distance = distance(point, landmark_pos)
+        if distance <= radius + robot_radius:
+            return True
+    return False
+
+
+def randConf():
+    return (random.uniform(-2000, 2000), random.uniform(-2000, 2000))
+
+
+def NEAREST_VERTEX(v, G):
+    min = distance(G[0], v)
+    minV = G[0]
+    for i in range(len(G)):
+        d = distance(v, G[i])
+        if d < min or i == 0:
+            min = d
+            minV = G[i]
+    return minV
+
+
+def Steer(q_near, q_rand, delta_q=300):
+    dx, dy = q_rand[0] - q_near[0], q_rand[1] - q_near[1]
+    d = distance(q_near, q_rand)
+
+    if d < delta_q:
+        return q_rand
+    else:
+        return (q_near[0] + delta_q * dx / d, q_near[1] + delta_q * dy / d)
+
+
+def buildRRT(landmarks, K, goal, delta_q=300):
+    start = (0, 0)
+    G = [start]
+    parent = {0: None}
+
+    goal_idx = None
+
+    for i in range(K):
+        q_rand = randConf()
+        q_near = NEAREST_VERTEX(q_rand, G)
+        q_new = Steer(q_near, q_rand, delta_q)
+        if in_collision(q_new, landmarks):
+            continue
+
+        G.append(q_new)
+        parent[len(G) - 1] = G.index(q_near)
+
+        if distance(q_new, goal) < delta_q:
+            goal_index = len(G) - 1
+            break
+
+    path = []
+    node = goal_index
+    while node is not None:
+        path.append(G[node])
+        node = parent[node]
+    path.reverse()
+
+    return path
+
+
+def follow_rrt_path(arlo, path):
+    for i in range(1, len(path)):
+        start = path[i - 1]
+        target = path[len(path) - i]
+
+        dx = target[0] - start[0]
+        dy = target[1] - start[1]
+        angle_rad = np.arctan2(dy, dx)
+        angle_deg = np.degrees(angle_rad)
+
+        arlo.rotate_robot(angle_deg)
+
+        distance_m = np.sqrt(dx**2 + dy**2) / 1000  # to meter
+
+        arlo.drive_forward_meter(distance_m, leftspeed=64, rightspeed=67)
+
+
+while running:
+    print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+    sleep(0.1)
+    print(arlo.stop())
+    print("Checking for landmark...")
+    landmark_detected, c, ids = checkForLandmark()
+    if landmark_detected:
+        print("Landmark detected! Stopping.")
+        print(arlo.stop())
+        rvecs, tvecs, objPoints = cv2.aruco.estimatePoseSingleMarkers(
+            c,
+            X,
+            intrinsic_matrix,
+            distortion_coeffs,
+        )
+        landmarks = []
+        id_list = []
+        for i in range(len(ids)):
+            id = ids[i][0]
+            if id in id_list:
+                continue
+
+            x = tvecs[i][0][0]
+            print(f"Landmark ID{id} is {cv2.norm(tvecs[i][0])} mm away from the camera")
+            y = tvecs[i][0][2]
+            landmarks.append((ids[i][0], x, y))
+            id_list.append(id)
+        goal = (0, 4000)
+        path = buildRRT(landmarks, 500, goal)
+        print("Path:", path)
+        follow_rrt_path(arlo, path)
+        running = False
+
+
+cam.stop()
+print("Finished")
