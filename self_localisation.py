2c365ed676253993b66ed384af3e9f9f8a5eb56a
vxf604 <vxf604@alumni.ku.dk>
2025-10-18 18:32:53 +0200

    alosette

--- self_localisation.py
+++ self_localisation.py
@@ -338,7 +338,10 @@ try:
             print("est_pose:", (est_pose.getX(), est_pose.getY(), est_pose.getTheta()))
             print("Distance to landmark center:", distance)
             
-            angle_diff = math.atan(est_pose.getTheta() - math.atan2(landmarkcenter[1], landmarkcenter[0]))
+            target_angle = math.atan2(math.atan2(landmarkcenter[1] -  est_pose.getY, 
+                                               landmarkcenter[0] - est_pose.getX()))
+            angle_diff = (target_angle - est_pose.getTheta() + math.pi) % (2 * math.pi) - math.pi
+            
             print("Angle difference:", angle_diff)
             print(arlo.rotate_robot(angle_diff))
             print(arlo.drive_forward_meter(distance/100.0))
