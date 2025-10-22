e1c5f611bc8a6f38136cf7ea58c635d629748e74
MariusGate3 <54096802+MariusGate3@users.noreply.github.com>
2025-10-22 17:15:31 +0200

    local

--- self_localisation2.py
+++ self_localisation2.py
@@ -449,9 +449,7 @@ try:
                     print("Seeing 2 landmarks, moving 1/4 distance")
                     distance_cm = distance_cm / 4
                     target_angle = math.atan2(dy, dx)
-                    angle_diff = (target_angle - est_pose.getTheta() + math.pi) % (
-                        2 * math.pi
-                    ) - math.pi
+                    angle_diff = target_angle - est_pose.getTheta()
                     print(f"Rotating {angle_diff} radians")
                     arlo.rotate_robot(math.degrees(angle_diff))
                     sleep(0.5)
@@ -461,9 +459,7 @@ try:
                 else:
                     print("Not seeing 2 landmarks, moving the rest of distance")
                     target_angle = math.atan2(dy, dx)
-                    angle_diff = (target_angle - est_pose.getTheta() + math.pi) % (
-                        2 * math.pi
-                    ) - math.pi
+                    angle_diff = target_angle - est_pose.getTheta()
                     print(f"Rotating {angle_diff} radians")
                     arlo.rotate_robot(math.degrees(angle_diff))
                     sleep(0.5)
