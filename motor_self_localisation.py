55da94f7676c71fa858a50b0dabe746a47283a8e
MariusGate3 <54096802+MariusGate3@users.noreply.github.com>
2025-10-29 21:52:42 +0100

    ads

--- motor_self_localisation.py
+++ motor_self_localisation.py
@@ -285,10 +285,10 @@ def motor_control(state, est_pose, target, seeing, seen2Landmarks):
     d = distance_to_target(est_pose, target)
 
     if state == "rotating":
-        step = max(8.0, min(abs(fi), 35.0))
-        turn = step if fi >= 0 else -step
+        # step = max(8.0, min(abs(fi), 35.0))
+        # turn = step if fi >= 0 else -step
         next_state = "forward" if abs(fi) < 5.0 else "rotating"
-        return ("rotate", turn), next_state
+        return ("rotate", fi), next_state
 
     if state == "forward":
         if not seeing and d < 40.0:
@@ -296,7 +296,7 @@ def motor_control(state, est_pose, target, seeing, seen2Landmarks):
         elif not seeing:
             return ("rotate", 20.0), "searching"
 
-        return ("forward", min(d, 40.0)), "forward"
+        return ("forward", min(d, 30.0)), "forward"
 
 
 # Main program #
