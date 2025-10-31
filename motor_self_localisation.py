fd2bdd9b95e032dfc6f5a024228c4fe2a0971f35
MariusGate3 <54096802+MariusGate3@users.noreply.github.com>
2025-10-31 18:31:21 +0100

    asda

--- motor_self_localisation.py
+++ motor_self_localisation.py
@@ -158,7 +158,7 @@ def translation1(p, transl1, std):
 
 def sample_motion_model(p, rot1, trans):
     if abs(rot1) > 0:
-        rotation(p, rot1, 0.15)
+        rotation(p, rot1, 0.10)
         p.setX(p.getX() + transerror(1))
         p.setY(p.getY() + transerror(1))
     elif trans > 0:
@@ -452,9 +452,6 @@ try:
             cmd, state = motor_control(state, est_pose, target, seeing, seen2Landmarks)
             execute_cmd(arlo, cmd)
             apply_motion_from_cmd(particles, cmd)
-            if state == "forward":
-                landmarks_seen.clear()
-                seen2Landmarks = False
         else:
             apply_sample_motion_model(particles, 0, 0)
 
