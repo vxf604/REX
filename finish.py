749f089b9ff6e408adb7319b3e239939754754b7
MariusGate3 <54096802+MariusGate3@users.noreply.github.com>
2025-11-02 19:45:31 +0100

    dasdas

--- finish.py
+++ finish.py
@@ -51,10 +51,10 @@ CBLACK = (0, 0, 0)
 
 # Landmarks.
 # The robot knows the position of 2 landmarks. Their coordinates are in the unit centimeters [cm].
-L1 = Landmark(x=0.0, y=0.0, color=CRED, ID=9, borderWidth_x=30, borderWidth_y=30)
-L2 = Landmark(x=0.0, y=300.0, color=CGREEN, ID=7, borderWidth_x=30, borderWidth_y=-30)
+L1 = Landmark(x=0.0, y=0.0, color=CRED, ID=1, borderWidth_x=30, borderWidth_y=30)
+L2 = Landmark(x=0.0, y=300.0, color=CGREEN, ID=2, borderWidth_x=30, borderWidth_y=-30)
 L3 = Landmark(x=400.0, y=0.0, color=CYELLOW, ID=3, borderWidth_x=-30, borderWidth_y=30)
-L4 = Landmark(x=400.0, y=300.0, color=CBLUE, ID=6, borderWidth_x=-30, borderWidth_y=-30)
+L4 = Landmark(x=400.0, y=300.0, color=CBLUE, ID=4, borderWidth_x=-30, borderWidth_y=-30)
 
 landmarks = [L1, L2, L3, L4]
 
